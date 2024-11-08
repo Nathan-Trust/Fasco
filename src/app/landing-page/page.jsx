e7e46144479bab1194927c9d0488ba9a8e83e21e
Nathan-Trust <nathantrustekanem@gmail.com>
2024-11-08 02:45:31 +0100

    seo-optimization

--- src/app/landing-page/page.jsx
+++ src/app/landing-page/page.jsx
@@ -1,9 +1,18 @@
 import HomeClient from "./client"
+import { Helmet } from 'react-helmet-async';
+
 
 const Home = () => {
   return (
-    <HomeClient/>
-  )
+    <>
+      <Helmet>
+        <title>Fasco|Home</title>
+        <meta name="description" content="Welcome to Fasco, your go-to platform for XYZ." />
+        <link rel="canonical" href="https://fasco-vercel.app/" />
+      </Helmet>
+      <HomeClient />
+    </>
+  );
 }
 
 export default Home
